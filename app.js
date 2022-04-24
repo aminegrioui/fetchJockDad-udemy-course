829d8b34b7a7793f5463d1debd597303922cb70f
amine <amine23grioui@gmail.com>
2022-04-24 15:24:35 +0200

    fetch joke data firt commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const url = 'https://icanhazdadjoke.com/ss';
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+
+btn.addEventListener('click', () => {
+  fetchDadJock();
+});
+
+const fetchDadJock = async () => {
+  try {
+    result.textContent = 'Loading ...';
+    const response = await fetch(url, {
+      headers: {
+        Accept: 'application/json',
+        'User-agent': 'app',
+      },
+    });
+    if (!response.ok) {
+      throw new Error('Error ... ');
+    }
+    const data = await response.json();
+    result.textContent = data.joke;
+  } catch (error) {
+    result.textContent = 'There was an error ...';
+  }
+};
+
+fetchDadJock();
